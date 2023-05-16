99c264ff9592006ea312921ba53c4dd288203b22
Onur <ob-vest@hotmail.com>
2023-05-16 18:56:56 +0200

    added more UI elements

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    <h2>Hej Sofie!</h2>
-    <h1>Velkommen</h1>
+    <div class="mt-20"></div>
+    <h3 class="text-gray-400">Hej Sofie!</h3>
+    <h1 class="pb-10 text-4xl">Velkommen tilbage!</h1>
+
+    <h2 class="my-2">Kategorier</h2>
+    <div class="grid grid-cols-3 gap-3">
+      <CategoryBox />
+      <CategoryBox />
+      <CategoryBox />
+    </div>
+    <div class="flex justify-between my-2">
+      <h2>Mest populære claims</h2>
+      <p>Se alle</p>
+    </div>
+    <div
+      class="bg-primary w-26 h-40 rounded-3xl justify-end items-center flex flex-col p-2"
+    >
+      <h1>Hel</h1>
+    </div>
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script></script>
---
+++ components/CategoryBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="bg-primary w-26 h-40 rounded-3xl justify-end items-center flex flex-col p-2"
+  >
+    <h3 class="text-black font-bold">Wellness</h3>
+    <div class="h-7 w-7 rounded-full bg-white items-center flex mt-2">
+      <img
+        class="h-3 w-full object-contain"
+        src="~/assets/icons/chevron-right.svg"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
