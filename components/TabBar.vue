146a387cc9e52ca0ce90a261718962a570016505
Onur <ob-vest@hotmail.com>
2023-05-22 23:50:35 +0200

    tab bar look done

---
+++ components/TabBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav class="bg-stone-950 w-full fixed bottom-0 py-4 px-5">
+      <div class="flex justify-between">
+        <img src="~/assets/icons/home.svg" alt="" />
+        <img src="~/assets/icons/offers.svg" alt="" />
+        <div
+          class="bg-primary p-3 rounded-full shadow-[0_4px_35px_-5px_rgba(0,0,0,0.3)] shadow-primary"
+        >
+          <img src="~/assets/icons/community.svg" alt="" />
+        </div>
+
+        <img src="~/assets/icons/reward.svg" alt="" />
+        <img src="~/assets/icons/profile.svg" alt="" />
+      </div>
+    </nav>
+  </div>
+</template>
