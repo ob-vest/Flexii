a48dd96c4c37bda97ffdf3744ac3c36c50c41f3b
Onur <ob-vest@hotmail.com>
2023-05-22 21:41:11 +0200

    added tab bar

--- components/CategoryBox.vue
+++ components/CategoryBox.vue
@@ -2,10 +2,15 @@
   <div
     class="bg-primary w-26 h-40 rounded-3xl justify-end items-center flex flex-col p-2"
   >
+    <img
+      class="h-full px-6 object-contain"
+      src="~/assets/illustrations/wellness.svg"
+      alt=""
+    />
     <h3 class="text-black font-bold">Wellness</h3>
     <div class="h-7 w-7 rounded-full bg-white items-center flex mt-2">
       <img
-        class="h-3 w-full object-contain"
+        class="p-2 h-full w-full object-contain"
         src="~/assets/icons/chevron-right.svg"
         alt=""
       />
