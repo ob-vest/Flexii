96d2bc2f7feda44909a3f591c30e3f4a39ebbe39
Onur <ob-vest@hotmail.com>
2023-05-25 21:46:26 +0200

    added new tab bar icons

--- components/TabBar.vue
+++ components/TabBar.vue
@@ -3,16 +3,22 @@
     <nav class="bg-stone-900 w-full fixed bottom-0 h-20 px-5 rounded-t-2xl">
       <div class="flex justify-between my-5">
         <img src="/icons/home.svg" alt="" />
-        <img src="/icons/offers.svg" alt="" />
+        <img class="notselected" src="/icons/community.svg" alt="" />
         <div
           class="bg-primary p-3 rounded-full shadow-[0_4px_35px_-5px_rgba(0,0,0,0.3)] shadow-primary"
         >
-          <img src="/icons/community.svg" alt="" />
+          <img src="/icons/joystick.svg" alt="" />
         </div>
 
-        <img src="/icons/reward.svg" alt="" />
-        <img src="/icons/profile.svg" alt="" />
+        <img class="notselected" src="/icons/star.svg" alt="" />
+        <img class="notselected" src="/icons/profile.svg" alt="" />
       </div>
     </nav>
   </div>
 </template>
+
+<style scoped>
+.notselected {
+  filter: brightness(50%);
+}
+</style>
